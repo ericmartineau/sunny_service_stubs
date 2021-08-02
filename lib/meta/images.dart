d19a175d818cb5af63aa34006ab1cd556df50968
ericmartineau <smartytime@gmail.com>
2021-08-02 14:32:43 -0700

    Adding image requirements

--- lib/meta/images.dart
+++ lib/meta/images.dart
@@ -1,3 +1,9 @@
-abstract class HasImage {}
+import 'package:sunny_service_stubs/models/waypoints/model/image_content.dart';
 
-abstract class HasImages {}
+abstract class HasImage {
+  IImageContent? get imageContent;
+}
+
+abstract class HasImages {
+  Iterable<IImageContent>? get imageList;
+}
